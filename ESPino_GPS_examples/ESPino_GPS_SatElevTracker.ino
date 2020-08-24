a1d87371bf110835e9b9f75f1ae2aed7e7c6819f
ThaiEasyElec <support@thaieasyelec.com>
2020-08-24 10:09:58 +0700

    Update ESPino_GPS_SatElevTracker.ino

--- ESPino_GPS_examples/ESPino_GPS_SatElevTracker.ino
+++ ESPino_GPS_examples/ESPino_GPS_SatElevTracker.ino
@@ -1,7 +1,7 @@
 #include <TinyGPS++.h>
 #include <SoftwareSerial.h>
 
-static const int RXPin = 0, TXPin = 2;
+static const int RXPin = 2, TXPin = 15;
 static const uint32_t GPSBaud = 9600;
 static const int MAX_SATELLITES = 40;
 static const int PAGE_LENGTH = 40;
